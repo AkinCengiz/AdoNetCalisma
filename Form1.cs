ca65d4415fb096fac32ecf9fdb526bfd797119ca
akincengiz <akincengiz@outlook.com.tr>
2022-11-13 14:20:29 +0300

    Dosyalar yüklendi

---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetDemos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ProductDal _productDal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            dgvProduct.DataSource = _productDal.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                Name = tbxName.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmount.Text)
+            });
+            LoadProducts();
+            Temizle();
+            MessageBox.Show("Product added!!!");
+        }
+        public void Temizle()
+        {
+            tbxName.Clear();
+            tbxStockAmount.Clear();
+            tbxUnitPrice.Clear();
+            tbxName.Focus();
+        }
+
+        private void dgvProduct_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxNameUpdate.Text = dgvProduct.CurrentRow.Cells[1].Value.ToString();
+            tbxUnipPriceUpdate.Text = dgvProduct.CurrentRow.Cells[2].Value.ToString();
+            tbxStockAmountUpdate.Text = dgvProduct.CurrentRow.Cells[3].Value.ToString();
+            tbxIdUpdate.Text = dgvProduct.CurrentRow.Cells[0].Value.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Product product = new Product
+            {
+                Id = Convert.ToInt32(dgvProduct.CurrentRow.Cells[0].Value),
+                Name = tbxNameUpdate.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnipPriceUpdate.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmountUpdate.Text)
+            };
+            _productDal.Update(product);
+            LoadProducts();
+            MessageBox.Show("Updated!");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dgvProduct.CurrentRow.Cells[0].Value);
+            _productDal.Delete(id);
+            LoadProducts();
+            MessageBox.Show("Deleted!");
+        }
+    }
+}
